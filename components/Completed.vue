5529ec6c2d98d4d2e3a77cf183b8ff8e52af92b0
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-01-05 15:53:47 +0800

    update css

--- components/Completed.vue
+++ components/Completed.vue
@@ -43,7 +43,7 @@ export default {
   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
+{
   transform: translateX(10px);
   opacity: 0;
 }
